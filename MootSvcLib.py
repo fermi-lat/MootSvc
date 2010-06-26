1a5834dac3a29c8030b71a7c8d53019d2177299c
jrb <>
2010-06-26 01:02:10 +0000

    add dependence on CalibData

--- MootSvcLib.py
+++ MootSvcLib.py
@@ -1,4 +1,4 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/MootSvc/MootSvcLib.py,v 1.1 2008/08/15 21:42:36 ecephas Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/MootSvc/MootSvcLib.py,v 1.2 2009/08/27 16:39:00 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['MootSvc'])
@@ -9,5 +9,6 @@ def generate(env, **kw):
     env.Tool('EventLib')
     env.Tool('LdfEventLib')
     env.Tool('mootCoreLib')
+    env.Tool('CalibDataLib')
 def exists(env):
     return 1;
